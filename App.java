da6ab481a969d774773504324c4bc8b071667405
surreptakos <djgatsakos@gmail.com>
2015-07-16 20:22:23 -0500

    a

---
+++ App.java
@@ -0,0 +1,17 @@
+public class App {
+
+	public static void main(String[] args) {
+		/* 10. 	Learn to touch type
+		 * 9. 	Name variables and subroutines descriptively
+		 * 8.	Type rather than read -- 10 minutes of typing is worth hours of reading
+		 * 7.	Write software that interests you
+		 * 6.	Read stack traces from the top line down
+		 * 5.	Aim to write the smallest working program possible -- finish the program!
+		 * 4.	Google like crazy
+		 * 3.	Build programs one step at a time -- incremental design
+		 * 2.	Ensure braces always pair up
+		 * 1.	Format code correctly	
+		 */
+	}
+
+}
